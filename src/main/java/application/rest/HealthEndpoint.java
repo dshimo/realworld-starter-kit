7d33f33f61414c0fd0b1436a5979e1b202df41f6
dshimo <dshi@utexas.edu>
2019-07-31 11:43:14 -0500

    health endpoint

---
+++ src/main/java/application/rest/HealthEndpoint.java
@@ -0,0 +1,20 @@
+package application.rest;
+
+// import javax.ws.rs.ApplicationPath;
+import javax.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.health.Health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+@Health
+@ApplicationScoped
+public class HealthEndpoint implements HealthCheck {
+
+    // TODO: Write a method to test connection to Derby
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("test").up().build();
+    }
+
+}
