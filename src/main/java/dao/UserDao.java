b73f4f37293dfd0d4f47a12ce06680ee3d51da99
dshimo <dshi@utexas.edu>
2019-12-16 18:20:47 -0600

    buildJson, deleteUser, //TODO cleanup

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import org.json.JSONObject;
 
 import core.user.User;
 
@@ -17,40 +18,62 @@ public class UserDao {
     public void createUser(User user) {
         System.out.println("Entering createUser");
         try {
-            // System.out.println(em);
-            System.out.println("User saving " + user);
             em.persist(user);
-            System.out.println("Success!");
         } catch(Exception e) {
-            System.out.println("------------------BEGIN STACK TRACE------------------");
             e.printStackTrace();
         }
         System.out.println("Exiting createUser");
     }
 
-    // public User findUser(String userID) {
-    //     return em.find(User.class, userID);
-    // }
+    public JSONObject buildJson(String userID) {
+        User traits = em
+            .createQuery("SELECT u FROM User u WHERE u.id = :userID", User.class)
+            .setParameter("userID", userID)
+            .getSingleResult();
+        return traits.toJson();
+    }
+
+    public User findUser(String userID) {
+        return em.find(User.class, userID);
+    }
 
     // public void updateUser(User user) {
     //     em.merge(user);
     // }
 
-    // public void deleteUser(User user) {
-    //     em.remove(user);
-    // }
-
-    // public User findByUsername(String username) {
-    //     return em.createNamedQuery("Users.findByUsername", User.class).setParameter("username", username)
-    //             .getSingleResult();
-    // }
-
-    public List<User> findAllUsers() {
-        return em.createNamedQuery("User.findAllUsers", User.class).getResultList();
+    public void deleteUser(String id) {
+        em.remove(em.find(User.class, id));
     }
 
-    // public User findByEmail(String email) {
-    //     return em.createNamedQuery("Users.findUser", User.class).setParameter("email", email).getSingleResult();
-    // }
+    public boolean userExists(String username) {      // em.contains does not work
+        return (Long) em.createQuery("SELECT COUNT(u.id) FROM User u WHERE u.username = :username")
+            .setParameter("username", username)
+            .getSingleResult() > 0;
+    }
+
+    public User findByUsername(String username) {
+        List<User> users = em.createQuery("SELECT u FROM User u WHERE u.username = :username", User.class)
+            .setParameter("username", username)
+            .getResultList();
+
+        return users.iterator().next();
+    }
+
+    public boolean emailExists(String email) {
+        return (Long) em.createQuery("SELECT COUNT(u.id) FROM User u WHERE u.email = :email")
+            .setParameter("email", email)
+            .getSingleResult() > 0;
+    }
+
+    public User findByEmail(String email) {
+        return em.createQuery("SELECT u FROM User u WHERE u.email = :email", User.class)
+            .setParameter("email", email)
+            .getSingleResult();
+    }
+
+    public List<User> findAllUsers() {
+        return em.createQuery("SELECT u.email, u.username, u.bio, u.image FROM User u")
+            .getResultList();
+    }
 
 }
