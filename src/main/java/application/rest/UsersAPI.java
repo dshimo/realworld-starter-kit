6a28f5b6b3f012153e0dce54629ba290586043a2
dshimo <dshi@utexas.edu>
2019-12-16 18:20:10 -0600

    UsersAPI create returns format

--- src/main/java/application/rest/UsersAPI.java
+++ src/main/java/application/rest/UsersAPI.java
@@ -16,9 +16,7 @@ import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
-import javax.print.attribute.standard.Media;
+import javax.json.Json;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 // import javax.servlet.http.HttpServletRequest;
@@ -26,6 +24,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -35,6 +34,7 @@ import com.ibm.websphere.security.jwt.InvalidClaimException;
 import com.ibm.websphere.security.jwt.JwtException;
 import com.ibm.websphere.security.jwt.KeyException;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import core.user.User;
@@ -71,41 +71,97 @@ public class UsersAPI {
     }
 
     @GET
-    @Path("/testToken")
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response token() throws JwtException, InvalidBuilderException, InvalidClaimException, KeyException {
-        String username = "david";
-        return Response.ok(tknGenerator.getToken(username)).build();
+    @Path("/buildJSON")
+    public Response test2() {
+        userDao.buildJson("1");
+        return Response.ok("See console.").build();
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Transactional
-    public Response createSimpleUser(Users users) {
+    public Response createSimpleUser(Users users)
+            throws JSONException, JwtException, InvalidBuilderException, InvalidClaimException, KeyException {
+        System.out.println("Creating simple user.");
         User user = users.getUser();
-        if (user.getUsername() == null || user.getPassword() == null || user.getEmail() == null) {
+        String username = user.getUsername();
+        String email = user.getEmail();
+
+        // Required fields
+        if (username == null || email == null || user.getPassword() == null) {
             return Response.status(Response.Status.BAD_REQUEST)
-                .entity("Request must contain a username, email, and password")
+                .entity("Username, email, or password must not be null or empty.")
                 .build();
         }
-        String output = "temporary";
-        System.out.println(user);
+
+        // Check duplicate username/email in database
+        if (userDao.userExists(username)) {
+            System.out.println("User exists!");
+            return Response.status(Response.Status.BAD_REQUEST)
+                .entity("Username already exists.")
+                .build();
+        }
+        if (userDao.emailExists(email)) {
+            System.out.println("Email exists!");
+            return Response.status(Response.Status.BAD_REQUEST)
+                .entity("Email already exists.")
+                .build();
+        }
+
         userDao.createUser(user);   // Persist
 
-        return Response.status(Response.Status.CREATED).entity(output).build();
+        JSONObject body = new JSONObject()
+            .put("users", addToken(userDao.findUser(user.getId()).toJson(), username));
+
+        return Response.status(Response.Status.CREATED).entity(body.toString()).build();
+    }
+
+    private JSONObject addToken(JSONObject json, String username)
+            throws JSONException, JwtException, InvalidBuilderException, InvalidClaimException, KeyException {
+        return json.put("token", tknGenerator.getToken(username));
     }
 
     @GET
     @Path("/getUser")
+    @Produces(MediaType.APPLICATION_JSON)
     @Transactional
     public Response returnUser() {
         List<User> users = userDao.findAllUsers();
-
-        System.out.println(users);
-        
         return Response.ok(users).build();
     }
+
+    // @GET
+    // @Path("/getUser/{username}")
+    // @Produces(MediaType.APPLICATION_JSON)
+    // @Transactional
+    // public Response loadUserInfo(@PathParam("username") String username) {
+    //     System.out.println("Received request to retrieve username: " + username);
+    //     User user = userDao.loadUserInfo(username);
+    //     System.out.println("User: " + user);
+    //     System.out.println("Pizzacat is not real.");
+    //     return Response.ok(user).build();
+    // }
+
+    @GET
+    @Path("/deleteUser/{id}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Transactional
+    public Response deleteUser(@PathParam("id") String id) {
+        userDao.deleteUser(id);
+        return Response.ok("Delete request received.").build();
+    }
+
+    // private Json buildBody() {
+    //     Json body = new Json();
+
+    // }
+
+    // private String produceToken(String username)
+    //         throws JwtException, InvalidBuilderException, InvalidClaimException, KeyException {
+    //     return tknGenerator.getToken(username);
+    // }
+
 
     /**
      * This method creates a new user from the submitted data (email, username,
@@ -162,11 +218,19 @@ public class UsersAPI {
     // private Map<String, Object> userResponse(AuthUser authUser) {
     //     return new HashMap<String, Object>() {
     //         private static final long serialVersionUID = 1L;
-
     //         {
     //             put("user", authUser);
     //         }
     //     };
+    // }
+
+    // @GET
+    // @Path("/testToken")
+    // @Produces(MediaType.TEXT_PLAIN)
+    // public Response token() throws JwtException, InvalidBuilderException,
+    // InvalidClaimException, KeyException {
+    // String username = "david";
+    // return Response.ok(tknGenerator.getToken(username)).build();
     // }
     
 }
